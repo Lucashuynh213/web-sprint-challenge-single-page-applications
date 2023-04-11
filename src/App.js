76c47435142423e1cb2eb80da269ead2a5f6b4c7
Lucas Huynh <nguyenhuynh21394@gmail.com>
2023-04-10 22:26:11 -0400

    got stuck

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { useState } from "react";
+import Form from "./components/Form";
+import { Route,Link, Switch } from "react-router-dom";
 
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
+
+function App() {
+  
+  return(
+    <div>
+      <nav>
+        <h1 className='store-header'>Bloomtech&apos;s Cafeteria</h1>
+        <div className='nav-links'>
+          <Link to="/">Home</Link>
+          <Link to="/help">Help</Link>
+        </div>
+      </nav>
+      <Switch>
+      <Route path={"/order-form"}>
+        <Form/>
+      </Route>  
+      <Route path="/">
+        <Header/>
+      </Route>
+    </Switch>
+    </div>
+  )
+}
+
 export default App;
